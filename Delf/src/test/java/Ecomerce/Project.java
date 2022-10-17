759d611e0b1d02cf3da35519fedf08ce0a6a63ad
Naveen <Naveen@LAPTOP-1KSOMSE2>
2022-10-17 23:22:32 +0530

    commited successfully

---
+++ Delf/src/test/java/Ecomerce/Project.java
@@ -0,0 +1,12 @@
+package Ecomerce;
+
+import org.testng.annotations.Test;
+
+public class Project {
+	@Test
+	public void pro1()
+	{
+		System.out.println("pro1 Started");
+	}
+
+}
---
+++ Delf/src/test/java/Ecomerce/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Naveen
+ *
+ */
+package Ecomerce;
